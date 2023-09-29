2640c05f618c3c5cfdf3d5f4d43d3a7a7336b08d
Nils Kohrs <8224528+nilskohrs@users.noreply.github.com>
2023-09-29 12:43:44 +0200

    Initial commit

---
+++ src/main/kotlin/bug/example/SupendErrorHandlingError.kt
@@ -0,0 +1,47 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponseException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class Controller {
+
+    @GetMapping("suspend")
+    suspend fun other() {
+        throw IllegalStateException("This returns double error response")
+    }
+}
+
+@ControllerAdvice
+class ExceptionHandler : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(Exception::class)
+    fun handleUnexpectedTechnicalException(
+        ex: Exception,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        return handleException(
+            ErrorResponseException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ProblemDetail.forStatus(500),
+                ex
+            ), request
+        )
+    }
+}
